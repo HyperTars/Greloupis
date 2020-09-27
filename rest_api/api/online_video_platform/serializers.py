521b1557dccdd3fea9d5cc565b7947aceb5f012c
FatBin <18813632558@163.com>
2020-09-28 03:19:16 +0800

    implemented the fundemental part of api about user

---
+++ rest_api/api/online_video_platform/serializers.py
@@ -0,0 +1,18 @@
+from flask_restplus import fields
+from rest_api.api.restplus import api
+
+user = api.model('User post', {
+    'user_name': fields.String(required=True, description='userName'),
+    'first_name': fields.String(required=True, description='firstName'),
+    'last_name': fields.String(required=True, description='lastName'),
+    'email': fields.String(required=True, description='email'),
+    'password': fields.String(required=True, description='password'),
+    'phone': fields.String(required=True, description='phone'),
+    'user_status': fields.String(required=True, description='userStatus'),
+})
+
+user_login = api.model('User login', {
+    'user_name': fields.String(required=True, description='userName'),
+    'email': fields.String(required=True, description='email'),
+    'password': fields.String(required=True, description='password'),
+})
