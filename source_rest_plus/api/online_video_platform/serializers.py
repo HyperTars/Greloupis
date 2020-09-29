e35fbbc8404490c5a07bb4bd99a0855ba024c3be
luoxuanbin <luoxuanbin@bytedance.com>
2020-09-30 02:03:54 +0800

    update the dependency

--- source_rest_plus/api/online_video_platform/serializers.py
+++ source_rest_plus/api/online_video_platform/serializers.py
@@ -1,4 +1,4 @@
-from flask_restplus import fields
+from flask_restx import fields
 from source_rest_plus.api.restplus import api
 
 user = api.model('User post', {
--- source_rest_plus/app.py
+++ source_rest_plus/app.py
@@ -19,10 +19,10 @@ def configure_app(flask_app):
     flask_app.config['SERVER_NAME'] = settings.FLASK_SERVER_NAME
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = settings.SQLALCHEMY_DATABASE_URI
     flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.SQLALCHEMY_TRACK_MODIFICATIONS
-    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
-    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
-    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
-    flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
+    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTX_SWAGGER_UI_DOC_EXPANSION
+    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPX_VALIDATE
+    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTX_MASK_SWAGGER
+    flask_app.config['ERROR_404_HELP'] = settings.RESTX_ERROR_404_HELP
 
 
 def initialize_app(flask_app):
