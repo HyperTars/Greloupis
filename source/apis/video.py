8ddf9575aaf85e5dbb2a02ad42bb8d6d3a3f9683
MikeYan01 <yly374779040@gmail.com>
2020-10-09 14:23:34 -0400

    finish API implementation

--- source/apis/video.py
+++ source/apis/video.py
@@ -4,13 +4,74 @@ from flask import Flask, request, g, Blueprint
 from flask_restx import Resource, Api, fields, marshal_with, reqparse, Namespace
 import json
 
+from .user import thumbnail, general_response, star, comment, like, dislike, \
+    star_response_list, comment_response_list, like_response_list, dislike_response_list
+
 video = Namespace('video', description='Video APIs')
 
+video_uri = video.model(name='VideoURI', model={
+    'video_low': fields.String,
+    'video_mid': fields.String,
+    'video_high': fields.String,
+})
+
+video_info = video.model(name='Video', model={
+    'video_id': fields.String,
+    'user_id': fields.String,
+    'video_title': fields.String,
+    'video_tag': fields.List(fields.String),
+    'video_category': fields.List(fields.String),
+    'video_description': fields.String,
+    'video_language': fields.String,
+    'video_status': fields.String,
+    'video_content': fields.String,
+    'video_status': fields.String,
+    'video_size': fields.Float,
+    'video_view': fields.Integer,
+    'video_like': fields.Integer,
+    'video_dislike': fields.Integer,
+    'video_comment': fields.Integer,
+    'video_star': fields.Integer,
+    'video_uri': fields.Nested(video_uri),
+    'video_thumbnail': fields.Nested(thumbnail),
+    'video_upload_date': fields.DateTime
+})
+
+view = video.model(name='View', model={
+    'video_id': fields.String,
+    'view_count': fields.Integer
+})
+
+view_response = video.model(name='ApiResponseWithView', model={
+    'code': fields.String,
+    'body': fields.Nested(view)
+})
+
+like_response = video.model(name='ApiResponseWithLike', model={
+    'code': fields.String,
+    'body': fields.Nested(like)
+})
+
+dislike_response = video.model(name='ApiResponseWithDislike', model={
+    'code': fields.String,
+    'body': fields.Nested(dislike)
+})
+
+star_response = video.model(name='ApiResponseWithStar', model={
+    'code': fields.String,
+    'body': fields.Nested(star)
+})
+
+comment_response = video.model(name='ApiResponseWithComment', model={
+    'code': fields.String,
+    'body': fields.Nested(comment)
+})
+
 @video.route('')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video information')
-@video.response(405, 'Method not allowed')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', video_info)
+@video.response(400, 'Invalid video information', general_response)
+@video.response(405, 'Method not allowed', general_response)
+@video.response(500, 'Internal server error', general_response)
 class Video(Resource):
 
     def post(self):
@@ -21,10 +82,10 @@ class Video(Resource):
 
 @video.route('/<string:video_id>')
 @video.param('video_id', 'Video ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID')
-@video.response(404, 'Video not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', video_info)
+@video.response(400, 'Invalid video ID', general_response)
+@video.response(404, 'Video not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoId(Resource):
 
     def get(self, video_id):
@@ -48,10 +109,10 @@ class VideoVideoId(Resource):
 
 @video.route('/<string:video_id>/view')
 @video.param('video_id', 'Video ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID')
-@video.response(404, 'Video not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', view_response)
+@video.response(400, 'Invalid video ID', general_response)
+@video.response(404, 'Video not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdView(Resource):
 
     def get(self, video_id):
@@ -69,10 +130,10 @@ class VideoVideoIdView(Resource):
 
 @video.route('/<string:video_id>/comment')
 @video.param('video_id', 'Video ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID')
-@video.response(404, 'Video not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', comment_response_list)
+@video.response(400, 'Invalid video ID', general_response)
+@video.response(404, 'Video not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdComment(Resource):
 
     def get(self, video_id):
@@ -84,10 +145,10 @@ class VideoVideoIdComment(Resource):
 @video.route('/<string:video_id>/comment/<string:user_id>')
 @video.param('video_id', 'Video ID')
 @video.param('user_id', 'User ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID  or user ID')
-@video.response(404, 'Video or user not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', comment_response)
+@video.response(400, 'Invalid video ID  or user ID', general_response)
+@video.response(404, 'Video or user not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdCommentUserId(Resource):
 
     def get(self, video_id, user_id):
@@ -119,10 +180,10 @@ class VideoVideoIdCommentUserId(Resource):
 
 @video.route('/<string:video_id>/dislike')
 @video.param('video_id', 'Video ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID')
-@video.response(404, 'Video not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', dislike_response_list)
+@video.response(400, 'Invalid video ID', general_response)
+@video.response(404, 'Video not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdDislike(Resource):
 
     def get(self, video_id):
@@ -134,11 +195,11 @@ class VideoVideoIdDislike(Resource):
 @video.route('/<string:video_id>/dislike/<string:user_id>')
 @video.param('video_id', 'Video ID')
 @video.param('user_id', 'User ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID or user ID')
-@video.response(404, 'Video or user not found')
-@video.response(405, 'Method not allowed')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', dislike_response)
+@video.response(400, 'Invalid video ID or user ID', general_response)
+@video.response(404, 'Video or user not found', general_response)
+@video.response(405, 'Method not allowed', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdDislikeUserId(Resource):
     
     def post(self, video_id, user_id):
@@ -155,10 +216,10 @@ class VideoVideoIdDislikeUserId(Resource):
 
 @video.route('/<string:video_id>/like')
 @video.param('video_id', 'Video ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID')
-@video.response(404, 'Video not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', like_response_list)
+@video.response(400, 'Invalid video ID', general_response)
+@video.response(404, 'Video not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdLike(Resource):
 
     def get(self, video_id):
@@ -170,11 +231,11 @@ class VideoVideoIdLike(Resource):
 @video.route('/<string:video_id>/like/<string:user_id>')
 @video.param('video_id', 'Video ID')
 @video.param('user_id', 'User ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID or user ID')
-@video.response(404, 'Video or user not found')
-@video.response(405, 'Method not allowed')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', like_response)
+@video.response(400, 'Invalid video ID or user ID', general_response)
+@video.response(404, 'Video or user not found', general_response)
+@video.response(405, 'Method not allowed', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdLikeUserId(Resource):
 
     def post(self, video_id, user_id):
@@ -191,10 +252,10 @@ class VideoVideoIdLikeUserId(Resource):
 
 @video.route('/<string:video_id>/star')
 @video.param('video_id', 'Video ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID')
-@video.response(404, 'Video not found')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', star_response_list)
+@video.response(400, 'Invalid video ID', general_response)
+@video.response(404, 'Video not found', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdStar(Resource):
 
     def get(self, video_id):
@@ -206,11 +267,11 @@ class VideoVideoIdStar(Resource):
 @video.route('/<string:video_id>/star/<string:user_id>')
 @video.param('video_id', 'Video ID')
 @video.param('user_id', 'User ID')
-@video.response(200, 'Successful operation')
-@video.response(400, 'Invalid video ID or user ID')
-@video.response(404, 'Video or user not found')
-@video.response(405, 'Method not allowed')
-@video.response(500, 'Internal server error')
+@video.response(200, 'Successful operation', star_response)
+@video.response(400, 'Invalid video ID or user ID', general_response)
+@video.response(404, 'Video or user not found', general_response)
+@video.response(405, 'Method not allowed', general_response)
+@video.response(500, 'Internal server error', general_response)
 class VideoVideoIdStarUserId(Resource):
 
     def post(self, video_id, user_id):
