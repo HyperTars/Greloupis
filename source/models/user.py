58919d432c67434c1f54c366755bf252f5e586f7
hypertars <hypertars@gmail.com>
2020-10-09 00:13:36 -0400

    add get User model & tests

--- source/models/user.py
+++ source/models/user.py
@@ -46,8 +46,55 @@ class User(db.Document):
     user_recent_login = db.ListField(db.EmbeddedDocumentField('LoginDetail'))
     user_following = db.ListField(db.StringField())
     user_follower = db.ListField(db.StringField())
-    
+
     meta = {'collection': 'user'}
+
+    def to_dict(self):
+        user_dict = {}
+        user_detail_dict = {}
+        user_thumbnail_dict = {}
+        user_recent_login_array = []
+        user_following_array = []
+        user_follower_array = []
+
+        user_detail_dict['user_first_name'] = self.user_detail.first_name or None
+        user_detail_dict['user_last_name'] = self.user_detail.last_name or None
+        user_detail_dict['user_phone'] = self.user_detail.phone or None
+        user_detail_dict['user_street1'] = self.user_detail.street1 or None
+        user_detail_dict['user_street2'] = self.user_detail.street2 or None
+        user_detail_dict['user_city'] = self.user_detail.city or None
+        user_detail_dict['user_state'] = self.user_detail.state or None
+        user_detail_dict['user_country'] = self.user_detail.country or None
+        user_detail_dict['user_zip'] = self.user_detail.zip or None
+
+        user_thumbnail_dict['user_thumbnail_uri'] = self.user_thumbnail.thumbnail_uri or None
+        user_thumbnail_dict['user_thumbnail_type'] = self.user_thumbnail.thumbnail_type or None
+
+        for login in self.user_recent_login:
+            temp_login_detail = {}
+            temp_login_detail['login_ip'] = login.login_ip
+            temp_login_detail['login_time'] = login.login_time
+            user_recent_login_array.append(temp_login_detail)
+        
+        for following in self.user_following:
+            user_following_array.append(following)
+
+        for follower in self.user_follower:
+            user_follower_array.append(follower)
+
+        user_dict['user_id'] = str(self._id)
+        user_dict['user_email'] = self.user_email
+        user_dict['user_name'] = self.user_name
+        user_dict['user_password'] = self.user_password 
+        user_dict['user_detail'] = user_detail_dict
+        user_dict['user_status'] = self.user_status
+        user_dict['user_thumbnail'] = user_thumbnail_dict
+        user_dict['user_reg_date'] = self.user_reg_date
+        user_dict['user_recent_login'] = user_recent_login_array
+        user_dict['user_following'] = user_following_array
+        user_dict['user_follower'] = user_follower_array
+        return user_dict
+    
 
     @property
     def password(self):
