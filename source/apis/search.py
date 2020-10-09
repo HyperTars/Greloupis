6900fd16c7c8e17aec9f78e4e4e650a0547ac5c8
MikeYan01 <yly374779040@gmail.com>
2020-10-09 14:23:34 -0400

    finish API implementation

--- source/apis/search.py
+++ source/apis/search.py
@@ -5,17 +5,23 @@ from flask_restx import Resource, Api, fields, marshal_with, reqparse, Namespace
 import json
 
 from .user import user_info, general_response
+from .video import video_info, general_response
 
 search = Namespace('search', description='Search APIs')
 
-user_response_list = search.model(name='ApiResponseWithList', model={
+user_response_list = search.model(name='ApiResponseWithUserList', model={
     'code': fields.String,
     'body': fields.List(fields.Nested(user_info))
 })
 
+video_response_list = search.model(name='ApiResponseWithVideoList', model={
+    'code': fields.String,
+    'body': fields.List(fields.Nested(video_info))
+})
+
 @search.route('/video?q=<string:keyword>')
 @search.param('keyword', 'Searching keyword')
-@search.response(200, 'Successfully got video search results.', general_response)
+@search.response(200, 'Successfully got video search results.', video_response_list)
 @search.response(400, 'Bad request.', general_response)
 @search.response(500, 'Internal server error.', general_response)
 class SearchVideo(Resource):
