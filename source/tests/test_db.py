708e0d99bdf346ac17b83c0d3df889a64435376b
hypertars <hypertars@gmail.com>
2020-10-09 00:13:36 -0400

    add get User model & tests

--- source/tests/test_db.py
+++ source/tests/test_db.py
@@ -56,6 +56,7 @@ class VideoOp(db.Document):
         video_op_dict['star_date'] = self.star_date
         return video_op_dict
 
+
 class UserDetail(db.EmbeddedDocument):
     first_name = db.StringField(max_length=50, required=True)
     last_name = db.StringField(max_length=50, required=True)
@@ -91,21 +92,70 @@ class User(db.Document):
     user_following = db.ListField(db.StringField())
     user_follower = db.ListField(db.StringField())
 
+    def to_dict(self):
+        user_dict = {}
+        user_detail_dict = {}
+        user_thumbnail_dict = {}
+        user_recent_login_array = []
+        user_following_array = []
+        user_follower_array = []
+
+        user_detail_dict['user_first_name'] = self.user_detail.first_name or None
+        user_detail_dict['user_last_name'] = self.user_detail.last_name or None
+        user_detail_dict['user_phone'] = self.user_detail.phone or None
+        user_detail_dict['user_street1'] = self.user_detail.street1 or None
+        user_detail_dict['user_street2'] = self.user_detail.street2 or None
+        user_detail_dict['user_city'] = self.user_detail.city or None
+        user_detail_dict['user_state'] = self.user_detail.state or None
+        user_detail_dict['user_country'] = self.user_detail.country or None
+        user_detail_dict['user_zip'] = self.user_detail.zip or None
+
+        user_thumbnail_dict['user_thumbnail_uri'] = self.user_thumbnail.thumbnail_uri or None
+        user_thumbnail_dict['user_thumbnail_type'] = self.user_thumbnail.thumbnail_type or None
+
+        for login in self.user_recent_login:
+            temp_login_detail = {}
+            temp_login_detail['login_ip'] = login.login_ip
+            temp_login_detail['login_time'] = login.login_time
+            user_recent_login_array.append(temp_login_detail)
+        
+        for following in self.user_following:
+            user_following_array.append(following)
+
+        for follower in self.user_follower:
+            user_follower_array.append(follower)
+
+        user_dict['user_id'] = str(self._id)
+        user_dict['user_email'] = self.user_email
+        user_dict['user_name'] = self.user_name
+        user_dict['user_password'] = self.user_password 
+        user_dict['user_detail'] = user_detail_dict
+        user_dict['user_status'] = self.user_status
+        user_dict['user_thumbnail'] = user_thumbnail_dict
+        user_dict['user_reg_date'] = self.user_reg_date
+        user_dict['user_recent_login'] = user_recent_login_array
+        user_dict['user_following'] = user_following_array
+        user_dict['user_follower'] = user_follower_array
+        return user_dict
+
+
 tu_detail = UserDetail(first_name="test_user", last_name="test_user", phone="+1xxxxxx", street1="str1", street2="str2", city="cty", state="stt", country="ctry", zip="zip")
 tu_thumbnail = Thumbnail(thumbnail_uri="test_uri", thumbnail_type="test_type")
 tu_login = LoginDetail(login_ip="1.1.1.1", login_time=parse("20201008153008"))
 tu = User(user_email="xx.gmail.com", user_name="test_user", user_password="askdkj091", user_detail=tu_detail, user_status="active", user_thumbnail=tu_thumbnail, user_reg_date=parse("20201008141231"), user_recent_login=[tu_login], user_following=["kl12j3lk12j3l12k"], user_follower=["89889a7d98as789d", "1h312jj3h12kj312h"])
-tu.save()
+# tu.save()
 
-print("test: get video_op")
+print("=============== test: get video_op ===============\n")
 vo = VideoOp.objects()
 for v in vo:
     print(v.to_dict())
+    print("\n")
 
-print("test: get user")
+print("=============== test: get user ===============\n")
 usr = User()
 usr = User.objects()
 len(usr)
 for u in usr:
-    print(u._id)
+    print(u.to_dict())
+    print("\n")
 
