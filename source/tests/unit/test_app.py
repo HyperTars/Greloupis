cf5da8e719d668e11c5a621dbda55552db391e52
MikeYan01 <yly374779040@gmail.com>
2020-10-12 19:27:12 -0400

    app.py and apiv1.py unit test

--- source/tests/unit/test_app.py
+++ source/tests/unit/test_app.py
@@ -1,20 +1,12 @@
 import unittest
+from source.app import *
 
-class TestStringMethods(unittest.TestCase):
+class TestApp(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_app(self):
+        self.assertIsNotNone(app, msg="Flask app initialization check")
+        self.assertEqual(app.name, 'source.app', msg="Flask app name check")
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
 
 if __name__ == '__main__':
     unittest.main()
