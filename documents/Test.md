d02173bbd0aae0d5d2e498540266a50cb8d63c88
hypertars <hypertars@gmail.com>
2020-10-12 18:22:48 -0400

    update test_models

--- documents/Test.md
+++ documents/Test.md
@@ -1,1 +1,9 @@
 - [Test Cases](https://docs.google.com/spreadsheets/d/1hoKAh89rNywF343tU5lzeamFmidxdYJ39CW_uUCCRLw/edit?usp=sharing)
+
+## Test Code
+
+### Unit Test
+
+- **test_db**
+
+- **test_service**
--- source/db/query_user.py
+++ source/db/query_user.py
@@ -24,6 +24,8 @@ def query_user_create(user_name: str, user_email: str, user_password: str, user_
 
     login = [LoginDetail(login_ip=user_ip, login_time=datetime.datetime.utcnow())]
 
+    # EmbeddedDocument must be included when creating
+    # user_detail, user_thumbnail, user_reg_date
     user = User(user_name=user_name, user_email=user_email, user_password=user_password,
                 user_status="private", user_detail=UserDetail(), user_thumbnail=Thumbnail(),
                 user_recent_login=login, user_reg_date=datetime.datetime.utcnow())
