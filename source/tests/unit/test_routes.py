4ef722e148f45ef9ea71033ad5bea48df5cbeda1
MikeYan01 <yly374779040@gmail.com>
2020-10-12 20:16:03 -0400

    api route unit test

--- source/tests/unit/test_routes.py
+++ source/tests/unit/test_routes.py
@@ -1,20 +1,47 @@
 import unittest
+from flask import Flask, request
+from source.routes.route_search import *
+from source.routes.route_user import *
+from source.routes.route_video import *
 
-class TestStringMethods(unittest.TestCase):
+app = Flask(__name__)
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+class TestRoutes(unittest.TestCase):
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_route(self):
+        '''
+            Test search user by keyword
+        '''
+        with app.test_request_context(
+            '/search/user?keyword=hypertars', data={}):
+            keyword = request.args.get('keyword')
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+            response_json = RouteSearchUser().get().get_json()
+            self.assertIsNotNone(response_json["body"], msg="Response json should contains body")
+            self.assertEqual(response_json["body"][0]["user_id"], "5f808f045e03b2165ca4275a", msg="First matched user id")
+            self.assertEqual(response_json["body"][0]["user_email"], "hypertars@gmail.com", msg="First matched user email")
+            self.assertEqual(response_json["body"][0]["user_name"], "hypertars", msg="First matched user name")
+
+
+        '''
+            Test search video by keyword
+        '''
+        with app.test_request_context(
+            '/search/video?keyword=xixihaha', data={}):
+            keyword = request.args.get('keyword')
+
+            response_json = RouteSearchVideo().get().get_json()
+            self.assertIsNotNone(response_json["body"], msg="Response json should contains body")
+            self.assertEqual(response_json["body"][0]["video_id"], "5f72999541bc583c4819d915", msg="First matched video id")
+            self.assertEqual(response_json["body"][0]["video_title"], "XiXiHaHa", msg="First matched video title")
+            self.assertEqual(response_json["body"][0]["video_raw_content"], "https://s3.amazon.com/54asd56a4d5asdasd.mp4", msg="First matched video content")
+
+
+    def test_user(self):
+        pass
+    
+    def test_video(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
