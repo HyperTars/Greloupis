3eea54619c672a206f8a64070aeb1a1dbef1e5b9
hypertars <hypertars@gmail.com>
2020-10-14 19:41:23 -0400

    update util_serializer.py

--- source/utils/util_serializer.py
+++ source/utils/util_serializer.py
@@ -13,17 +13,23 @@ def util_serializer_mongo_results_to_array(results):
 def util_serializer_array_dict_to_json(array_dict):
     res = []
     for d in array_dict:
-        res.append(json.dumps(d, cls=JSONDateEncoder))
+        res.append(util_serializer_dict_to_json(d))
     return res
 
 
+def util_serializer_dict_to_json(d):
+    return json.dumps(d, cls=JSONDateEncoder)
+
+
 def util_serializer_request(request, format="dict"):
-    return request.to_dict()
+    if format == "json":
+        return util_serializer_dict_to_json(request.to_dict())
+    else:
+        return request.to_dict()
 
 
 def util_serializer_api_response(body, code):
-    response = {}
-    response['body'] = body
+    response = {'body': body}
     result = json.dumps(response, indent=4, sort_keys=True, separators=(',', ': ')) \
         .replace('\\"', "'").replace('"{', '{').replace('}"', '}').replace("'", '"')
     return Response(result, status=code, mimetype='application/json')
@@ -33,7 +39,7 @@ class JSONDateEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
             return obj.strftime('%Y-%m-%d %H:%M:%S')
-        elif isinstance(obj, date):
+        elif isinstance(obj, datetime.date):
             return obj.strftime("%Y-%m-%d")
         else:
             return json.JSONEncoder.default(self, obj)
