b1e1cd2e123cc9dc7fae114d4f066959b29951ff
MikeYan01 <yly374779040@gmail.com>
2020-10-14 18:03:08 -0400

    add more test entries

--- source/test.py
+++ source/test.py
@@ -1,9 +1,17 @@
 import unittest
 from source.settings import *
 from source.tests.unit.test_models import TestModels
+from source.tests.unit.test_routes import TestRoutes
+from source.tests.unit.test_app import TestApp
+from source.tests.unit.test_apiv1 import TestApiV1
 
 
 if __name__ == '__main__':
     conf = config['test']
+    
+    TestApp()
+    TestApiV1()
     TestModels()
+    TestRoutes()
+    
     unittest.main()
