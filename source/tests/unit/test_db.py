7b7968ec896ceb1002482d3eaa627b3336bfdb48
hypertars <hypertars@gmail.com>
2020-10-15 13:56:13 -0400

    add MongoError & update query_user

--- source/tests/unit/test_db.py
+++ source/tests/unit/test_db.py
@@ -2,11 +2,20 @@ import unittest
 from source.settings import *
 from source.db.mongo import get_db
 from source.tests.unit.test_load_data import *
+from source.db.query_user import *
+import random
 
 
 class TestUser(unittest.TestCase):
     def setUp(self):
         self.data = util_load_test_data()
+
+    def test_user_create(self):
+        user_name = "dev_user_temp"
+        user_email = "dev_temp@gmail.com"
+        user_password = "dev"
+        self.assertEqual(query_user_create(user_name=user_name, user_email=user_email,
+                                           user_password=user_password).user_name, user_name)
 
 
 class TestVideo(unittest.TestCase):
