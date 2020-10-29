9a8add235860d37fbc10e90a2a4d173389d9290f
MikeYan01 <yly374779040@gmail.com>
2020-10-29 10:20:48 -0400

    init React App and fix package.json error

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 class App extends Component {
 
   render() {
     return (
-        <div>
-            <h1>Frontend under construction</h1>
+        // <div>
+        //     <h1>Frontend under construction</h1>
+        // </div>
+      <Router>
+        <div className="App">
+          <h1>Frontend under construction</h1>
         </div>
+      </Router>
     );
   }
 }
