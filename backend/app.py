a4bff93bf7e009d913a2c5acaa05301027470d2d
HyperTars <hypertars@gmail.com>
2020-10-31 11:12:04 -0400

    Add nginx support, fix Flask-CORS support. Now we can access frontend by http://localhost

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
-from flask import Flask
+from flask import Flask, request
 from apiv1 import blueprint
 from settings import config
 from flask_cors import CORS
@@ -10,9 +10,10 @@ import os
 # from flask import request, redirect, session
 
 app = Flask(__name__)
-CORS(app, resources={r'/*': {'origins': config['test'].FRONTEND}},
-     supports_credentials=True)
-
+app.config.from_object(config['test'])
+app.register_blueprint(blueprint)
+# CORS(app, resources={r'/*': {'origins': config['test'].FRONTEND}},
+#      supports_credentials=True)
 """
 @app.before_request
 def before_request():
@@ -26,7 +27,22 @@ def before_request():
         return redirect("/user/login")
 """
 
+
+@app.after_request
+def add_cors_headers(response):
+    r = request.referrer[:-1]
+    if r in config['test'].FRONTEND:
+        response.headers.add('Access-Control-Allow-Origin', r)
+        response.headers.add('Access-Control-Allow-Credentials', 'true')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Headers', 'Cache-Control')
+        response.headers.add('Access-Control-Allow-Headers',
+                             'X-Requested-With')
+        response.headers.add('Access-Control-Allow-Headers', 'Authorization')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET, POST, OPTIONS, PUT, DELETE')
+    return response
+
+
 if __name__ == '__main__':
-    app.config.from_object(config['test'])
-    app.register_blueprint(blueprint)
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,15 +7,30 @@ class App extends Component {
     // let LOCAL_BASE_URL = "127.0.0.1"
     let TEST_BASE_URL = "0.0.0.0"
 
-    let url = `http://${TEST_BASE_URL}:5000/user/5f88f883e6ac4f89900ac983`
+    let url1 = `http://${TEST_BASE_URL}:5000/user/5f88f883e6ac4f89900ac983`
 
-    fetch(url, {credentials: "include"})
-      .then(response => response.json());
+    fetch(url1, {credentials: "include"})
+        .then(response => response.json())
+        .then((responseData) => {
+              console.log(responseData);
+              return responseData;
+            });
+
+    let url2 = `http://${TEST_BASE_URL}:5000/search/video?keyword=ih`
+
+    fetch(url2, {credentials: "include"})
+        .then(response => response.json())
+        .then((responseData) => {
+              console.log(responseData);
+              return responseData;
+            });
 
     return (
       <Router>
         <div className="App">
-          <h1>Frontend under construction</h1>
+            <h1>Frontend under construction</h1>
+            <div> check console </div>
+            <div>{this.res}</div>
         </div>
       </Router>
     );
