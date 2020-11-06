d8f637b543143494768a6e81e4e3bb3a04231ade
MikeYan01 <yly374779040@gmail.com>
2020-11-06 00:00:04 -0500

    add App Header, and implement basic search function (#24)
    * add App Header, and implement basic search function
    
    * minor endpoint modification

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+import "../static/css/App.css";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+  }
+
+  render() {
+    const logoPath = "/Assets/logo.svg";
+    const uploadPath = "/Assets/upload.svg";
+    const avatarPath = "/Assets/avatar.jpg";
+
+    return (
+      <nav className="header">
+        <Link to="/home">
+          <div className="header-logo">
+            <img className="header-logo__img" src={logoPath} alt="Logo"></img>
+            <span className="tooltip header-searchBar__button-tooltip">
+              Home Page
+            </span>
+          </div>
+        </Link>
+
+        <form className="header-searchBar" action="/search">
+          <input
+            className="header-searchBar__input"
+            type="text"
+            name="keyword"
+            placeholder="Search"
+            value={this.state.value}
+            onChange={(e) => {
+              this.setState({
+                value: e.target.value,
+              });
+            }}
+          ></input>
+
+          <div className="header-searchBar__button">
+            <button className="header-searchBar__button-icon" type="submit">
+              <img src="/Assets/search.svg" alt="Search Icon"></img>
+            </button>
+
+            <span className="tooltip header-searchBar__button-tooltip">
+              Search
+            </span>
+          </div>
+        </form>
+
+        <div className="header-profile">
+          <Link to="/home">
+            <div className="header-profile__upload">
+              <img
+                className="header-profile__upload-icon"
+                src={uploadPath}
+                alt="Content Upload Button"
+              ></img>
+              <span className="tooltip header-tooltip-text">
+                Upload a new video
+              </span>
+            </div>
+          </Link>
+
+          <Link to="/home">
+            <div className="header-profile__avatar">
+              <img
+                className="header-profile__avatar-img"
+                src={avatarPath}
+                alt="Profile Avatar"
+              ></img>
+              <span className="tooltip header-tooltip-text">View Profile</span>
+            </div>
+          </Link>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
---
+++ frontend/src/components/SearchResult.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { searchUser, searchVideo } from "./FetchData";
+
+function SearchResult({ endpoint }) {
+  const [loading, setLoading] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [userResult, setUserResult] = useState(null);
+  const [videoResult, setVideoResult] = useState(null);
+
+  let keyword =
+    window.location.search.substring(1) &&
+    window.location.search.substring(1).split("=").length > 1
+      ? window.location.search.substring(1).split("=")[1]
+      : "";
+
+  useEffect(() => {
+    searchUser(endpoint, keyword)
+      .then((res) => {
+        if (res == null) {
+          return;
+        }
+        setLoading(false);
+        setUserResult(res.body);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setErrorMsg(e.message);
+      });
+  }, [endpoint, keyword]);
+
+  useEffect(() => {
+    searchVideo(endpoint, keyword)
+      .then((res) => {
+        if (res == null) {
+          return;
+        }
+        setLoading(false);
+        setVideoResult(res.body);
+      })
+      .catch((e) => {
+        setLoading(false);
+        setErrorMsg(e.message);
+      });
+  }, [endpoint, keyword]);
+
+  const loadingFormat = (
+    <div className="topMargin">
+      <div>Loading......</div>
+    </div>
+  );
+
+  const errorFormat = (
+    <div className="topMargin">
+      <div>Error: {errorMsg}</div>
+    </div>
+  );
+
+  const sampleFormat = (
+    <div className="topMargin">
+      <div>
+        Search user title:{" "}
+        {!userResult
+          ? "Loading"
+          : userResult.length === 0
+          ? "No matched result"
+          : userResult[0]["user_name"]}
+      </div>
+
+      <div>
+        Search video title:{" "}
+        {!videoResult
+          ? "Loading"
+          : videoResult.length === 0
+          ? "No matched result"
+          : videoResult[0]["video_title"]}
+      </div>
+    </div>
+  );
+
+  return loading ? loadingFormat : errorMsg ? errorFormat : sampleFormat;
+}
+
+export default SearchResult;
