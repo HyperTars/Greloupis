9669ffb6415f568d7bc6518f83f8b14cabe5ee2e
MikeYan01 <yly374779040@gmail.com>
2020-11-05 23:07:25 -0500

    add App Header, and implement basic search function

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,16 +2,23 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { TEST_ENDPOINT } from "./Endpoint";
 import Dashboard from "./Dashboard";
+import Header from "./Header";
+import SearchResult from "./SearchResult";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <Header />
           <Switch>
             <Route
-              path=""
+              path="/home"
               render={() => <Dashboard endpoint={TEST_ENDPOINT} />}
+            />
+            <Route
+              path="/search"
+              render={(props) => <SearchResult endpoint={TEST_ENDPOINT} />}
             />
           </Switch>
         </div>
