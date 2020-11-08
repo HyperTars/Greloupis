e6ee9a03177e2bcedfafaebbfb844a7d77e000f2
MikeYan01 <yly374779040@gmail.com>
2020-11-08 14:54:48 -0500

    (WIP) signin and signup pages (#27)

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,27 +1,53 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { TEST_ENDPOINT /*, LOCAL_ENDPOINT*/ } from "./Endpoint";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from "./Dashboard";
 import Header from "./Header";
 import SearchResult from "./SearchResult";
-
-const CURRENT_ENDPOINT = TEST_ENDPOINT;
+import Login from "./Login";
+import Register from "./Register";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <Header />
           <Switch>
             <Route
               path="/home"
-              render={() => <Dashboard endpoint={CURRENT_ENDPOINT} />}
+              render={() => (
+                <div>
+                  <Header />
+                  <Dashboard />
+                </div>
+              )}
             />
             <Route
               path="/search"
-              render={(props) => <SearchResult endpoint={CURRENT_ENDPOINT} />}
+              render={(props) => (
+                <div>
+                  <Header />
+                  <SearchResult />
+                </div>
+              )}
             />
+            <Route
+              exact
+              path="/login"
+              component={Login}
+              render={(props) => <Login />}
+            />
+            <Route
+              path="/register"
+              component={Register}
+              render={(props) => <Register />}
+            />
+            <Route path="/" render={() => <Redirect to="/home"></Redirect>} />
           </Switch>
         </div>
       </Router>
---
+++ frontend/src/components/Register.js
@@ -0,0 +1,225 @@
+import React /*, {Component}*/ from "react";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import AuthService from '../service/AuthService';
+import PropTypes from "prop-types";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" target="_blank" href="https://github.com/HyperTars/Online-Video-Platform/">
+        Greloupis
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const styles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
+
+class Register extends  React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      success: false,
+      message: ""
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      username: event.state.username,
+      email: event.state.email,
+      password: event.state.password,
+    })
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({
+      message: "",
+      success: false
+    });
+
+    // this.form.validateAll();
+
+    if (true) { // validate here
+      AuthService.register(
+        this.state.username, 
+        this.state.email, 
+        this.state.password
+      ).then(
+        response => {
+          this.setState({
+            message: response.message,
+            success: true
+          });
+        },
+        error => {
+          const resMessage = 
+            (error.response &&
+              error.response.date &&
+              error.response.data.message) ||
+              error.message ||
+              error.toString();
+          this.setState({
+            success: false,
+            message: resMessage
+          });
+
+          console.log(this.state.message);
+          alert(this.state.message);
+        }
+      );      
+    } else {
+      this.setState({
+        loading: false,
+      });
+      alert("Invalid params");
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  autoComplete="username"
+                  name="username"
+                  required
+                  fullWidth
+                  id="username"
+                  label="User Name"
+                  autoFocus
+                  value={this.state.username}
+                  onChange={(event) =>
+                    this.setState({
+                      [event.target.name]: event.target.value
+                    })
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  autoComplete="email"
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="User Email"
+                  autoFocus
+                  value={this.state.email}
+                  onChange={(event) =>
+                    this.setState({
+                      [event.target.name]: event.target.value
+                    })
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  value={this.state.password}
+                  onChange={(event) =>
+                    this.setState({
+                      [event.target.name]: event.target.value
+                    })
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive news and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }
+}
+
+Register.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default new withStyles(styles)(Register);
