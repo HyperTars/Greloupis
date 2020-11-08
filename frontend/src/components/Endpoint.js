220ecdb7876bb51ba51a4a0ef16b7457b68362a1
HyperTars <hypertars@gmail.com>
2020-11-08 18:13:32 -0500

    add heroku background endpoint back

--- frontend/src/components/Endpoint.js
+++ frontend/src/components/Endpoint.js
@@ -1,5 +1,4 @@
-// export const TEST_ENDPOINT = "https://greloupis-backend.herokuapp.com";
+export const TEST_ENDPOINT = "https://greloupis-backend.herokuapp.com";
 export const BACKEND_HTTPS_ENDPOINT = "https://greloupis-backend.herokuapp.com";
 export const BACKEND_HTTP_ENDPOINT = "http://greloupis-backend.herokuapp.com";
 export const LOCAL_ENDPOINT = "http://localhost:5000";
-export const TEST_ENDPOINT = "http://localhost:5000";
