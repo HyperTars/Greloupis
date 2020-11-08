3649b5edae8b8365390f37993efb6a9705f28df9
HyperTars <hypertars@gmail.com>
2020-11-08 10:30:50 -0500

    Xuanbin backend (#28)
    * update gitingore file
    
    * add the expired response
    
    * fix make lint
    
    * add router test case
    
    Co-authored-by: luoxuanbin <luoxuanbin@bytedance.com>

--- backend/utils/util_jwt.py
+++ backend/utils/util_jwt.py
@@ -1,5 +1,11 @@
+from flask import jsonify
+
 blacklist = set()
 
-def util_show_blacklist():
-    print(blacklist)
-    return True
+
+def util_get_formated_response(code=200, data={}, msg=""):
+    return jsonify({
+        "code": code,
+        "data": data,
+        "msg": msg
+    })
