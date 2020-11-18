fce7f8bae8d82f9193d220246bb9cb80bc37f0ca
HyperTars <hypertars@gmail.com>
2020-11-18 09:20:43 -0500

    update endpoint

--- frontend/src/components/Endpoint.js
+++ frontend/src/components/Endpoint.js
@@ -2,5 +2,5 @@ export const HEROKU_ENDPOINT = "https://greloupis-backend.herokuapp.com";
 export const LOCAL_ENDPOINT = "http://localhost:5000";
 
 export function backendPoint() {
-    return LOCAL_ENDPOINT;
+    return HEROKU_ENDPOINT;
 }
