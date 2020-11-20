aa4fee9ed4e33113ef505e80547395d446249a4f
HyperTars <hypertars@gmail.com>
2020-11-20 13:55:06 -0500

    update set-env

--- frontend/src/components/Endpoint.js
+++ frontend/src/components/Endpoint.js
@@ -2,5 +2,7 @@ export const HEROKU_ENDPOINT = "https://greloupis-backend.herokuapp.com";
 export const LOCAL_ENDPOINT = "http://localhost:5000";
 
 export function backendPoint() {
-    return HEROKU_ENDPOINT;
+    if (!process.env.PROFILE || process.env.PROFILE === "prod")
+        return HEROKU_ENDPOINT;
+    return LOCAL_ENDPOINT;
 }
