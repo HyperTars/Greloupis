c67983dc0ecbaa5759643357706c54498f1cbb63
HyperTars <hypertars@gmail.com>
2020-11-27 12:24:04 -0500

    AWS (#66)
    * add aws endpoint
    
    * aws post key
    
    * update aws post
    
    * update aws uri
    
    * update aws uri
    
    * update aws uri
    
    * update aws config
    
    * update video format
    
    * video upload and update
    
    * update streaming uri
    
    * update video page when still pending
    
    Co-authored-by: MikeYan01 <yly374779040@gmail.com>

--- backend/configs/config_base.py
+++ backend/configs/config_base.py
@@ -1,5 +1,4 @@
 import os
-# import sys
 
 
 class BaseConfig(object):
@@ -21,6 +20,16 @@ class BaseConfig(object):
                  'dislike', 'dislikes', 'video_dislike',
                  'star', 'stars', 'video_star',
                  'share', 'shares', 'video_share']
+
+    # AWS
+    AWS_AUTH_KEY = "bf9e5788a7f59072e26712bdfacdfd30c0941d836e9" \
+        "340de9503ca579aa765716ae9521ae429d76876b818b00c34096582" \
+        "ac4c7784eaf2c1febdafd667134101"
+    AWS_CLOUD_FRONT = "https://d2t7530wn5sgoq.cloudfront.net/assets/MP4"
+    AWS_STREAMING_FORMAT = ".mp4"
+    AWS_STREAMING_LOW = "360"
+    AWS_STREAMING_MID = "720"
+    AWS_STREAMING_HIGH = "1080"
 
     @staticmethod
     def init_app(app):
