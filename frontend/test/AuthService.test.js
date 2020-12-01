0ff8e14c0152737f35d95699f74ef1b784cf1430
MikeYan01 <yly374779040@gmail.com>
2020-12-01 16:28:30 -0500

    try fix

--- frontend/test/AuthService.test.js
+++ frontend/test/AuthService.test.js
@@ -1,6 +1,17 @@
 import AuthService from "../src/service/AuthService";
 require("jest-localstorage-mock");
 
+const OLD_ENV = process.env;
+
+beforeEach(() => {
+  jest.resetModules(); // most important - it clears the cache
+  process.env = { ...OLD_ENV }; // make a copy
+});
+
+afterAll(() => {
+  process.env = OLD_ENV; // restore old env
+});
+
 test("is auth", () => {
   const result = AuthService.isAuth();
   expect(result).toBe(false);
@@ -13,6 +24,8 @@ test("get current user test", () => {
 });
 
 test("login", async () => {
+  process.env.NODE_ENV = "production";
+
   await AuthService.login("hypertars", "hypertars");
   expect(localStorage.getItem("user_id")).toBe('"5f88f883e6ac4f89900ac983"');
   expect(localStorage.getItem("user_name")).toBe('"hypertars"');
