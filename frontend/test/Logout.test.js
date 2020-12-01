9c751fa9dd2db84cf275d273933e2b83019e24d2
MikeYan01 <yly374779040@gmail.com>
2020-12-01 11:53:26 -0500

    util test

---
+++ frontend/test/Logout.test.js
@@ -0,0 +1,27 @@
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+test("log out should clear local storage", () => {
+  expect(global.localStorage.getItem("user_id")).toBe(null);
+});
--- frontend/test/util.test.js
+++ frontend/test/util.test.js
@@ -1,6 +1,32 @@
 import * as func from "../src/util";
+import moment from "moment";
+
+test("convert timestamp to relative time", () => {
+  const result = func.convertToRelativeTime("2020-01-01");
+  expect(result).toBe(moment("2020-01-01").startOf("minutes").fromNow());
+});
+
+test("convert second to hour:minute:second format", () => {
+  const result = func.secondTimeConvert(3661);
+  expect(result).toBe("01:01:01");
+});
+
+test("convert timestamp to yy:mm:dd", () => {
+  const result = func.dateConvert("2020-01-01 12:00:00");
+  expect(result).toBe("2020-01-01");
+});
 
 test("getSubstr should slice the first and last character of the string", () => {
   const result = func.getSubstr("'abc'");
   expect(result).toBe("abc");
+});
+
+test("set the max display length of given string", () => {
+  const result = func.ellipsifyStr("abcdefghijkl", 5);
+  expect(result).toBe("abcde...");
+});
+
+test("generate uuid", () => {
+  const result = func.uuid();
+  expect(result.length).toBe(22);
 });
