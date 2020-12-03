71ddae223e1507fc8d75a58638a2f7fd46f74c92
HyperTars <hypertars@gmail.com>
2020-12-03 11:08:28 -0500

    fix frontend errors (#80)
    * update make heroku
    
    * token expire redirecting
    
    Co-authored-by: MikeYan01 <yly374779040@gmail.com>

--- frontend/src/components/ErrorPage.js
+++ frontend/src/components/ErrorPage.js
@@ -2,7 +2,6 @@ function ErrorPage(errCode) {
   if (errCode["errCode"] === "403") window.location.href = "/403";
   else if (errCode["errCode"] === "404") window.location.href = "/404";
   else if (errCode["errCode"].startsWith("50")) window.location.href = "/500";
-  else window.location.href = "/404";
 
   return;
 }
