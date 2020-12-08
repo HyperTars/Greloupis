e7ffe2fc6ef8175c52b6493a14e15bdfc26a19ca
HyperTars <hypertars@gmail.com>
2020-12-08 09:35:24 -0500

    frontend docs (#84)
    * fix GITHUB_REPO link
    
    * add frontend images

--- frontend/src/components/Endpoint.js
+++ frontend/src/components/Endpoint.js
@@ -1,6 +1,6 @@
 export const HEROKU_ENDPOINT = "https://greloupis-backend.herokuapp.com";
 export const LOCAL_ENDPOINT = "http://localhost:5000";
-export const GITHUB_REPO = "https://https://github.com/HyperTars/Online-Video-Platform";
+export const GITHUB_REPO = "https://github.com/HyperTars/Online-Video-Platform";
 export const S3_RAW_VIDEO_BUCKET = "https://vod-watchfolder-ovs-lxb.s3-us-west-1.amazonaws.com/";
 export const S3_AVATAR_BUCKET = "https://greloupis-images.s3.amazonaws.com/avatar-";
 export const S3_VIDEO_THUMBNAIL_BUCKET = "https://greloupis-images.s3.amazonaws.com/thumbnail-";
